40d3690f79f021efe13dbc7b068bd691ee246c94
LinuxScriptingsh <39629672+LinuxScriptingsh@users.noreply.github.com>
2018-06-18 21:00:50 +0200

    Add files via upload

---
+++ OriginScriptInstaller.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+echo "###################################################"
+echo Made by LinuxScripting.sh. This script comes WITHOUT warranty.
+echo Inspired by LinuxGuides. https://www.youtube.com/LinuxGuides
+echo Please check the code of the script before executing this Script.
+echo You can abort this script every time by pressing  Ctrl + C
+echo "###################################################"
+echo This Script will now delete ~/Origin32/ "("If existing")"
+echo [Enter]
+echo "###################################################"
+
+read
+
+rm -r ~/Origin32/
+
+
+echo "###################################################"
+echo Do you want, that this installer removes all other Versions of wine on your Computer?
+echo [y/n]
+echo "###################################################"
+read a
+if [[ $a == "Y" || $a == "y" ]]; then
+	sudo apt purge *wine* --yes
+fi
+
+sudo dpkg --add-architecture i386
+wget https://dl.winehq.org/wine-builds/Release.key
+sudo apt-key add Release.key
+#!/bin/bash
+echo "Which OS are you using? (Hint: Wine for Ubuntu 'Bionic' 18.04 isn't available yet)"
+options=("Linux Mint 18.x" "Linux Mint 17.x" "Ubuntu")
+select opt in "${options[@]}"
+	do
+	case $opt in
+		"Linux Mint 18.x")
+		sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
+		break
+		;;
+		"Linux Mint 17.x")
+		sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ trusty main'
+            	break
+            	;;
+		"Ubuntu")
+		sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+		break
+		;;
+        	*) 
+		echo invalid option
+		;;
+		esac
+	done
+sudo apt-get update
+
+sudo apt-get install --install-recommends winehq-staging --yes
+
+wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+
+chmod +x winetricks
+
+WINEPREFIX=~/.Origin32 WINEARCH=win32 wine
+WINEPREFIX=~/.Origin32 ./winetricks vcrun2010 vcrun2013 vcrun2017 d3dx9
+WINEPREFIX=~/.Origin32 wine winecfg
+
+wget https://origin-a.akamaihd.net/Origin-Client-Download/origin/live/OriginThinSetup.exe
+WINEPREFIX=~/.Origin32 wine OriginThinSetup.exe
+rm OriginThinSetup.exe
+rm winetricks
+rm Release.key
+
+echo "###################################################"
+echo Installer has just finished. 
+echo Start game with following Command: 
+echo WINEPREFIX=~/.Origin32 wine "C:/Program Files (x86)/Origin/Origin.exe"
+echo Good Luck and Have Fun!
+echo "###################################################"
